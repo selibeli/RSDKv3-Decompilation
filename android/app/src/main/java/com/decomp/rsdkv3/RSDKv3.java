4b61def62e17f79c476a99bf4a938b83b4c36f01
selibeli <115391206+selibeli@users.noreply.github.com>
2026-08-12 21:22:41 -0700

    Fix repeated Android storage permission prompt

--- android/app/src/main/java/com/decomp/rsdkv3/RSDKv3.java
+++ android/app/src/main/java/com/decomp/rsdkv3/RSDKv3.java
@@ -2,6 +2,7 @@ package com.decomp.rsdkv3;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Environment;
 
 import java.io.File;
@@ -34,10 +35,16 @@ public class RSDKv3 extends SDLActivity {
         }
     }
 
-    public String getBasePath() {
-        Context c = getApplicationContext();
-        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-        String p = Environment.getExternalStorageDirectory().getAbsolutePath() + "/RSDK/v3";
+   public String getBasePath() {
+    Context c = getApplicationContext();
+    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        requestPermissions(new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, 0);
+    }
+    String p = Environment.getExternalStorageDirectory().getAbsolutePath() + "/RSDK/v3";
         //getExternalStorageDirectory is deprecated. I do not care.
         new File(p).mkdirs();
         return p + "/";
